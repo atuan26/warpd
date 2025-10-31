e043b37b24761140233e67d026305ae76365ad1c
atuan26 <tuannaa26@gmail.com>
2025-10-31 18:31:26 +0700

    fix windows.mk

--- mk/windows.mk
+++ mk/windows.mk
@@ -2,11 +2,21 @@
 CC=x86_64-w64-mingw32-gcc
 CXX=x86_64-w64-mingw32-g++
 CFLAGS+=-DWINDOWS -mwindows
-CXXFLAGS+=-DWINDOWS -mwindows -I/mingw64/include/opencv4
-LDFLAGS+=-luser32 -lgdi32 -lole32 -loleaut32 -luuid -lstdc++ -mwindows -lopencv_imgproc -lopencv_core
+CXXFLAGS+=-DWINDOWS -mwindows
+LDFLAGS+=-luser32 -lgdi32 -lole32 -loleaut32 -luuid -lstdc++ -mwindows
 
-CFILES=$(shell find src/*.c src/windows/*.c src/platform/windows/*.c)
-CXXFILES=$(shell find src/platform/windows/*.cpp) src/common/opencv_detector.cpp
+# Add OpenCV support if available
+ifndef DISABLE_OPENCV
+	CXXFLAGS+=-I/mingw64/include/opencv4
+	LDFLAGS+=-lopencv_imgproc -lopencv_core
+	OPENCV_FILES=src/common/opencv_detector.cpp src/platform/windows/opencv_detector.cpp
+else
+	OPENCV_FILES=
+endif
+
+# Basic working file lists - explicit to avoid find command issues
+CFILES=src/config.c src/daemon.c src/grid.c src/grid_drw.c src/hint.c src/histfile.c src/history.c src/input.c src/mode-loop.c src/mouse.c src/normal.c src/screen.c src/scroll.c src/smart_hint.c src/warpd.c src/windows/main.c src/windows/stubs.c src/platform/windows/filemon.c src/platform/windows/ui_detector.c src/platform/windows/windows.c src/platform/windows/winscreen.c
+CXXFILES=src/platform/windows/uiautomation_detector.cpp $(OPENCV_FILES)
 OBJFILES=$(CFILES:.c=.o) $(CXXFILES:.cpp=.o)
 
 ifeq ($(CC), cl.exe)
@@ -29,14 +39,12 @@ endif
 %.o: %.cpp
 	$(CXX) -c $< -o $@ $(CXXFLAGS)
 
-# Special rule for common OpenCV file
-src/common/opencv_detector.o: src/common/opencv_detector.cpp
-	$(CXX) -c $< -o $@ $(CXXFLAGS)
-
 all: $(OBJFILES)
 	-mkdir -p bin
 	$(CXX) -o bin/warpd.exe $(OBJFILES) $(LDFLAGS)
-	#$(CC) /Fe:bin/warpd.exe $(OBJFILES) user32.lib gdi32.lib
+
 clean:
-	rm $(OBJFILES)
-	rm -rf bin
+	-rm -f $(OBJFILES)
+	-rm -rf bin
+
+.PHONY: all clean
